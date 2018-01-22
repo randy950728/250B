4d2945566e3ce537ef4bbda0836aac1c808a3aac
Unknown <randy950728@yahoo.com.tw>
2018-01-22 14:28:19 -0800

    Project 1 Done

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -14,11 +14,11 @@ test_image,  test_label  = mndata.load_testing()		#Read testing image and label
 #Varaible Definition
 #-------------------------#
 global num_per_digit
-
 orig_train_size = len(train_image)
 test_size		= len(test_image)
-sample_size 	= int(orig_train_size/6*0.5)
+sample_size 	= int(orig_train_size/6/5*0.5)
 num_per_digit 	= int(sample_size/10)
+
 
 #Euclidean Distance between 2 function
 #-------------------------#
@@ -28,23 +28,7 @@ def distance(a,b):
 	sum_of_sqr = (a_q-b_q)**2
 	result = np.sum(sum_of_sqr)**0.5
 	return result
-	# sum_of_sqr= 0
-	# for i in range(len(a)):
-	# 	sum_of_sqr+=(a[i]-b[i])**2
-	# result = sum_of_sqr**0.5
-	# return result
 
-#Merge sort sampling function
-#-------------------------#
-# def merge_sort(train_image):
-# 	if(len(train_image)==1):
-# 		return train_image
-# 	else:
-# 		pivot = len(train_image)/2
-# 		left_side 	= merge_sort(train_image[0:pivot])
-# 		right_side	= merge_sort(train_image[pivot:len(train_image)])
-# 		sorted_train= []
-# 		while(len(left_side)!=0 and len(right_side)!=0):
 
 #Merge sort sampling function
 #-------------------------#
@@ -57,36 +41,23 @@ def sort(train_image):
 
 	for i in range(num_pixel):
 		avg_train[i]=float(sum(train_image[:][i]))/float(len(train_image))
-	print(avg_train)
+
 	for i in range(num_image):
 		all_diff[i] = (train_image[i]-avg_train)**2
 
 	for i in range(num_image):
 		sum_diff[i] = np.sum(all_diff[i,:])**0.5
 	result = [(sum_diff[i], train_image[i]) for i in range(num_image)]
-	reversed(sorted(result))
+	result.sort(key=lambda x: x[0])
 	return result
-	# diff_array = np.zeros((len(train_image),len(train_image)))
-	# for i in range(len(train_image)):
-	# 	for j in range(len(train_image)):
-	# 		print(i,j)
-	# 		if(j>=i):
-	# 			continue
-	# 		else:
-	# 			dist = distance(train_image[i],train_image[j])
-	# 			diff_array[i,j]= dist
-	# 			diff_array[j,i]= dist
-	# unique_array = []
-	# for i in range(len(train_image)):
-	# 	unique_array.append((np.sum(diff_array[i,:]),train_image[i]))
-	# sorted_array=sorted(unique_array)
-	# print(sorted_array)
+
 
 #function that finds the closest index given the value in a sorted array
 #-------------------------#
 def find_index(val, array):
-	idx = (np.abs(array-value)).argmin()
+	idx = (np.abs(array-val)).argmin()
 	return idx
+
 
 #Merge sort sampling function
 #-------------------------#
@@ -95,8 +66,9 @@ def merge_sampling(train_image, train_label):
 	sub_image =[]
 	sub_label =[]
 	distrib=0.30
-	normal_distrib = [0.3415, 0.1359, 0.0214, 0.0013]
-	
+	# normal_distrib = [0.3415, 0.1359, 0.0214, 0.0013]
+	normal_distrib = [0.3415, 0.1359, 0.0214+0.0013]
+
 	# Split the the input image into 10 digits
 	cat_train_image = [[] for i in range(10)]
 	for i in range(len(train_image)):
@@ -104,6 +76,7 @@ def merge_sampling(train_image, train_label):
 		cat_train_image[num_group].append(train_image[i])
 
 	for i in range(10):
+		# Aquire the training digit sorted by eclidean dist.
 		result = sort(cat_train_image[i])
 		res_size = len(result)
 
@@ -112,39 +85,94 @@ def merge_sampling(train_image, train_label):
 		avg_diff = np.average(diff_inf)
 		std_diff = np.std(diff_inf)
 
-		# locate boundary
-		mean_idx = find_index(avg_diff, diff_inf)
-		po_std_idx = find_index(avg_diff+std_diff, diff_inf)
-		pt_std_idx = find_index(avg_diff+2*std_diff, diff_inf)
-		mo_std_idx = find_index(avg_diff-std_diff, diff_inf)
-		mt_std_idx = find_index(avg_diff-2*std_diff, diff_inf)
+		# locate STD boundary
+		pos_std_idx = [find_index(avg_diff+j*std_diff, diff_inf) for j in range(0,4)]
+		neg_std_idx = [find_index(avg_diff-j*std_diff, diff_inf) for j in range(0,4)]
+		neg_std_idx[0]=neg_std_idx[0]-1 # offset by 1 to avoid repeat sampling
 
-		fit = stats.norm.pdf(test, np.mean(test), np.std(test))
-		pyplot.hist(test,normed=True)
-		pyplot.show()
+		# Calculate number of samples per deviation
+		samp_per_std = [int(normal_distrib[j]*num_per_digit) for j in range(3)]
 
-		# print("test",i,test)
+		# Calculate sample spacing
+		space_a = [int((pos_std_idx[j+1]-pos_std_idx[j])/samp_per_std[j]) for j in range(3)]
+		space_b = [int((pos_std_idx[j+1]-pos_std_idx[j])/samp_per_std[j]) for j in range(3)]
 
+		# Take samples from each deviation
+		for j in range(3):
+			curr_idx = pos_std_idx[j]
+			count=0
+			while(count<= samp_per_std[j]):
+				sub_image.append(result[curr_idx][1])
+				sub_label.append(i)
+				curr_idx+=space_a[j]
+				count+=1
 
-		for j in range(int(num_per_digit*distrib)):
-			sub_image.append(result[res_size-1-j][1])
-			sub_label.append(i)
+		for j in range(3):
+			curr_idx = neg_std_idx[j]
+			count=0
+			while(count<= samp_per_std[j]):
+				sub_image.append(result[curr_idx][1])
+				sub_label.append(i)
+				curr_idx-=space_b[j]
+				count+=1
 
-		for j in range(int(num_per_digit*(1-distrib))):
-			if(res_size/2)>int(num_per_digit*(1-distrib)):
-				sub_image.append(result[res_size/2-j][1])
-			else:
-				offset = num_per_digit*(1-distrib)-len(res_size)/2
-				sub_image.append(result[res_size/2+offset-j][1])
-			sub_label.append(i)
+		# fit = stats.norm.pdf(test, np.mean(test), np.std(test))
+		# pyplot.hist(diff_inf,normed=True)
+		# pyplot.show()
+
+		# Print Some statis
+		# print("samp_per_std", samp_per_std)
+		# print(diff_inf)
+		# print("avg_diff", avg_diff)
+		# print("std_diff", std_diff)
+		# print("pos_std_idx", pos_std_idx)
+		# print("neg_std_idx", neg_std_idx)
+		# print(diff_inf[pos_std_idx[0]])
+		# print("space_a", space_a)
+		# print("space_b", space_b)
+		# print(" ")
+		# # print("test",i,test
 	return sub_image,sub_label
+
 
 # Printing some info #
 #-------------------------#
 print("input size", len(train_image))
 print("test size", len(test_image))
 print("smaple size", sample_size)
-# print(mndata.display(train_image[1]))
+
+
+#1-NN function
+#-------------------------#
+def onn(item, train_image, train_label):
+	nn_val=1000000
+	nn_label=-1
+	for i in range(len(train_image)):
+		dist = distance(item,train_image[i])
+		if(dist<nn_val):
+			nn_val = dist
+			nn_label = train_label[i]
+	return nn_label
+
+
+#Testing function
+#-------------------------#
+def test_model(train_set, train_label, test_image, test_label):
+	model = NearestNeighbors(n_neighbors=1, algorithm='kd_tree',metric='euclidean').fit(train_set)
+	# dist, predict_index = model.kneighbors(test_image)
+	correct=0
+	for i in range(len(test_image)):
+		if(i%1000==0):
+			print(i)
+		# predict = onn(test_image[i],train_set, train_label)
+		dist, index = model.kneighbors([test_image[i]])
+		index = index[0][0]
+		# index = predict_index[i]
+		if(train_label[index]==test_label[i]):
+			correct+=1
+	rate = float(correct)/float(len(test_image))
+	return rate
+
 
 # Baseline sampling
 #-------------------------#
@@ -160,7 +188,6 @@ while(curr_size<sample_size):
 	del(index_array[index_index])
 	curr_size+=1
 
-print(len(baseline_train_image))
 
 # Data sampling
 #-------------------------#
@@ -168,37 +195,60 @@ index_array = range(0,orig_train_size)
 new_image, new_label = merge_sampling(train_image, train_label)
 print(len(new_image), len(baseline_train_image))
 
-#1-NN function
+# Model Testing
 #-------------------------#
-def onn(item, train_image, train_label):
-	nn_val=1000000
-	nn_label=-1
-	for i in range(len(train_image)):
-		dist = distance(item,train_image[i])
-		if(dist<nn_val):
-			nn_val = dist
-			nn_label = train_label[i]
-
-	return nn_label
-
-#Testing function
-#-------------------------#
-def test_model(train_set, train_label, test_image, test_label):
-	model = NearestNeighbors(n_neighbors=1, algorithm='kd_tree',metric='euclidean').fit(train_set)
-	# dist, predict_index = model.kneighbors(test_image)
-	correct=0
-	for i in range(len(test_image)):
-		print(i)
-		# predict = onn(test_image[i],train_set, train_label)
-		dist, index = model.kneighbors([test_image[i]])
-		index = index[0][0]
-		# index = predict_index[i]
-		if(train_label[index]==test_label[i]):
-			correct+=1
-	rate = float(correct)/float(len(test_image))
-	return rate
-
 # base = test_model(baseline_train_image,baseline_train_label, test_image,test_label)
 new  = test_model(new_image,new_label, test_image,test_label)
 # print("baseline model "+str(base))
 print("new model " + str(new))
+
+
+# Scrapped code
+#-------------------------------------------------------------#
+# for j in range(int(num_per_digit*distrib)):
+# 	sub_image.append(result[res_size-1-j][1])
+# 	sub_label.append(i)
+
+# for j in range(int(num_per_digit*(1-distrib))):
+# 	if(res_size/2)>int(num_per_digit*(1-distrib)):
+# 		sub_image.append(result[res_size/2-j][1])
+# 	else:
+# 		offset = num_per_digit*(1-distrib)-len(res_size)/2
+# 		sub_image.append(result[res_size/2+offset-j][1])
+# 	sub_label.append(i)
+
+
+# diff_array = np.zeros((len(train_image),len(train_image)))
+# for i in range(len(train_image)):
+# 	for j in range(len(train_image)):
+# 		print(i,j)
+# 		if(j>=i):
+# 			continue
+# 		else:
+# 			dist = distance(train_image[i],train_image[j])
+# 			diff_array[i,j]= dist
+# 			diff_array[j,i]= dist
+# unique_array = []
+# for i in range(len(train_image)):
+# 	unique_array.append((np.sum(diff_array[i,:]),train_image[i]))
+# sorted_array=sorted(unique_array)
+# print(sorted_array)
+
+
+# sum_of_sqr= 0
+# for i in range(len(a)):
+# 	sum_of_sqr+=(a[i]-b[i])**2
+# result = sum_of_sqr**0.5
+# return result
+
+#Merge sort sampling function
+#-------------------------#
+# def merge_sort(train_image):
+# 	if(len(train_image)==1):
+# 		return train_image
+# 	else:
+# 		pivot = len(train_image)/2
+# 		left_side 	= merge_sort(train_image[0:pivot])
+# 		right_side	= merge_sort(train_image[pivot:len(train_image)])
+# 		sorted_train= []
+# 		while(len(left_side)!=0 and len(right_side)!=0):
