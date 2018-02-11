298d86b7d11d1bf7dc81395ebe429ccdb56d0aee
Unknown <randy950728@yahoo.com.tw>
2018-02-10 17:22:53 -0800

    about to add pa2

--- HW3/hw1.py
+++ HW3/hw1.py
@@ -1,17 +1,15 @@
 from mnist import MNIST
-# import scipy.stats as stats
-# from matplotlib import pyplot
-# import random
-# from sklearn.neighbors import NearestNeighbors
 import numpy as np
+import random
 from scipy.stats import multivariate_normal
+from matplotlib  import pyplot
 
 
 # File Reading #
 #-------------------------#
 mndata = MNIST('data')									#Specify data directory
 raw_image,	raw_label 	= mndata.load_training()		#Read training image and label
-test_image,	test_label  = mndata.load_testing()		#Read testing image and label
+test_image,	test_label  = mndata.load_testing()			#Read testing image and label
 
 #Varaible Definition
 #-------------------------#
@@ -34,36 +32,102 @@ valid_label = raw_label[train_size+1: raw_size]
 num_digit=np.zeros(10)
 for i in train_label:
 	num_digit[i]+=1
-
 prob_digit = num_digit/len(train_label)
 
 
 # Classify each training data point
 #-------------------------#
-cat_digit=[]
-for i in range(10):
-	cat_digit.append([])
-
+cat_digit=[[] for i in range(10)]
 for i in range(len(train_label)):
 	curr_digit=train_label[i]
 	cat_digit[curr_digit].append(train_image[i])
-for i in range(10):
-	cat_digit[i] = np.asarray(cat_digit[i])
 
+
+# Convert training data of each digit into numpy array
+#-------------------------#
+cat_digit = [np.asarray(cat_digit[i]) for i in range(10)]
+
+
+# Calculate mean and covaraince of each digit
+#-------------------------#
 digit_mean=[]
-digit_cov=[]
+digit_covv=[]
 for i in range(10):
 	mean = np.mean(cat_digit[i],axis=0)
 	cov  = np.cov(cat_digit[i].T)
 	digit_mean.append(mean)
-	digit_cov.append(cov)
-c=0.5
-identity = np.identity(len(train_image[0]))
-identity = c*identity
+	digit_covv.append(cov)
 
-digit_cov = [digit_cov[i]+identity for i in range(10)]
+
+# smoothing covariance matrix
+#-------------------------#
+# C=[5062,5092,5125,5187]
+C=range(2000,4000,50)
+print(C)
+# c=5100
+c=3350
+result =[]
+
+ci= c*np.identity(len(train_image[0]))
+digit_cov = [digit_covv[i]+ci for i in range(10)]
+
+
+# Classify each training data point
+#-------------------------#
+digit_norm=[]
 for i in range(10):
-	prob =np.log(multivariate_normal.pdf(train_image[0],digit_mean[i],digit_cov[i]))
-	print(prob)
-print(train_label[0])
+	digit_norm.append( multivariate_normal(digit_mean[i],digit_cov[i]))
 
+
+# Classify each training data point
+#-------------------------#
+correct=0.
+for i in range(len(valid_label)):
+	temp = np.zeros(10)
+	for j in range(10):
+		temp[j] = digit_norm[j].logpdf(valid_image[i]) + np.log(prob_digit[j])
+
+	best_guess = np.argmax(temp)
+	# print(temp)
+	# print(valid_label[i],best_guess)
+	if(best_guess==valid_label[i]):
+		correct+=1
+
+	if(i%1000==0):
+		print(i)
+
+
+# Classify each training data point
+#-------------------------#
+acc = correct/len(valid_label)
+print("C="+str(c)+"  error:"+str(1-acc))
+# result.append(1-acc)
+
+# pyplot.plot(C,result)
+# pyplot.title("Validation set error rate VS. covariance smoothing constant-c")
+# pyplot.xlabel("smoothing constant-c")
+# pyplot.ylabel("Validation set error rate")
+# pyplot.show()
+# quick = np.asarray(result)
+# print("argmax",np.argmax(result))
+
+# Test on testing set
+#-------------------------#
+correct=0.
+for i in range(len(test_label)):
+	temp = np.zeros(10)
+	for j in range(10):
+		temp[j] = digit_norm[j].logpdf(test_image[i]) + np.log(prob_digit[j])
+
+	best_guess = np.argmax(temp)
+	# print(temp)
+	# print(valid_label[i],best_guess)
+	if(best_guess==test_label[i]):
+		correct+=1
+
+	if(i%1000==0):
+		print(i)
+# Classify each training data point
+#-------------------------#
+acc = correct/len(valid_label)
+print("Test result "+"C="+str(c)+"  error:"+str(1-acc))
